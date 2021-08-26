6897fa697f19736c7fa823f85a4d63ea54d60a49
afeiship <1290657123@qq.com>
2021-08-26 09:20:19 +0800

    chore: dist rm from git

--- build/scripts.js
+++ build/scripts.js
@@ -4,14 +4,14 @@
   var gulp = require('gulp');
   var pkgJson = require('../package.json');
   var $ = require('gulp-load-plugins')({
-    pattern: ['gulp-*', 'gulp.*', 'del', '@feizheng/gulp-*']
+    pattern: ['gulp-*', 'gulp.*', 'del', '@jswork/gulp-*']
   });
 
   gulp.task('scripts', function() {
     return gulp
       .src(['src/index.js'], { allowEmpty: true })
       .pipe($.umd({ namespace: () => 'EventMitt', exports: () => 'EventMitt' }))
-      .pipe($.feizheng.pkgHeader())
+      .pipe($.jswork.pkgHeader())
       .pipe(gulp.dest('dist'))
       .pipe($.size({ title: '[ default size ]:' }))
       .pipe($.uglify())
