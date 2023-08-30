04f81dd550c6e423152ff3bb3186178d8f0102aa
afeiship <1290657123@qq.com>
2023-08-30 10:16:30 +0800

    feat: add typing

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+interface EventMittOnOptions {
+  once?: boolean;
+  immidiate?: boolean;
+}
+
+interface EventMitt {
+  on(name: string, handler: Function, options?: EventMittOnOptions): void;
+  off(name: string, handler: Function): void;
+  emit(name: string, ...args: any[]): void;
+  one(name: string, handler: Function): void;
+  once(name: string, handler: Function): void;
+  upon(name: string, handler: Function): void;
+  on2immidiate(name: string, handler: Function): void;
+}
